7a24e9bebc3fd19a4904678097233e0db317021f
Matthias Schmitt <matthias.schmitt@scoop-software.de>
2014-07-06 16:14:42 +0200

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require("gulp"),
-  path = require("path"),
-  gutil = require("gulp-util"),
-  rename = require("gulp-rename"),
-  pkg = require("./package.json");
+path = require("path"),
+gutil = require("gulp-util"),
+rename = require("gulp-rename"),
+pkg = require("./package.json"),
+changed = require("gulp-changed");
 
 var SRC  = "app";
 
@@ -31,9 +32,9 @@ var MAIN_SCRIPT = "index.js";
 // Compile app/less sources in CSS and auto-prefix the CSS
 function compileLess() {
   return gulp.src(SRC_LESS_ALL)
-    .pipe(require("gulp-less")({ paths: [ path.join(SRC_LESS_BASE, "includes") ] }))
-    .pipe(require("gulp-autoprefixer")("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-    .pipe(gulp.dest(DIST_LESS));
+  .pipe(require("gulp-less")({ paths: [ path.join(SRC_LESS_BASE, "includes") ] }))
+  .pipe(require("gulp-autoprefixer")("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
+  .pipe(gulp.dest(DIST_LESS));
 }
 
 gulp.task("compile:less", ["update"], function() {
@@ -43,9 +44,9 @@ gulp.task("compile:less", ["update"], function() {
 // Minify the CSS
 gulp.task("dist:less", ["update"], function() {
   return compileLess()
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(require('gulp-minify-css')())
-    .pipe(gulp.dest(DIST_LESS));
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(require('gulp-minify-css')())
+  .pipe(gulp.dest(DIST_LESS));
 });
 
 
@@ -55,10 +56,10 @@ gulp.task("dist:less", ["update"], function() {
 function compileJavaScript() {
   var jshint = require("gulp-jshint");
   return gulp.src(SRC_JAVASCRIPT_ALL)
-    .pipe(jshint())
-    .pipe(jshint.reporter(require('jshint-stylish')))
-    .pipe(require("gulp-concat")(MAIN_SCRIPT))
-    .pipe(gulp.dest(DIST_JAVASCRIPT));
+  .pipe(jshint())
+  .pipe(jshint.reporter(require('jshint-stylish')))
+  .pipe(require("gulp-concat")(MAIN_SCRIPT))
+  .pipe(gulp.dest(DIST_JAVASCRIPT));
 }
 
 gulp.task("compile:javascript", ["update"], function() {
@@ -68,32 +69,32 @@ gulp.task("compile:javascript", ["update"], function() {
 // Uglify the JS
 gulp.task("dist:javascript", ["update"], function() {
   return compileJavaScript()
-    .pipe(rename({ suffix: ".min" }))
+  .pipe(rename({ suffix: ".min" }))
     .pipe(require('gulp-ngmin')()) // ngmin makes angular injection syntax compatible with uglify
     .pipe(require("gulp-uglify")())
     .pipe(gulp.dest(DIST_JAVASCRIPT));
-});
+  });
 
 
 // Images
 
 gulp.task("compile:images", ["update"], function() {
   return gulp.src(SRC_IMAGES_ALL)
-    .pipe(gulp.dest(DIST_IMAGES));
+  .pipe(gulp.dest(DIST_IMAGES));
 });
 
 // Compress the images
 gulp.task("dist:images", ["update"], function() {
   return gulp.src(SRC_IMAGES_ALL)
-    .pipe(require("gulp-imagemin")({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest(DIST_IMAGES));
+  .pipe(require("gulp-imagemin")({ optimizationLevel: 3, progressive: true, interlaced: true }))
+  .pipe(gulp.dest(DIST_IMAGES));
 });
 
 
 // Copy the html assets without modification
 gulp.task("compile:html", ["update"], function() {
   return gulp.src(SRC_HTML)
-    .pipe(gulp.dest(DIST));
+  .pipe(gulp.dest(DIST));
 });
 
 
@@ -103,16 +104,16 @@ gulp.task("dist:html", ["update"], function() {
   var replace = require('gulp-replace');
 
   return gulp.src(SRC_HTML)
-      .pipe(replace(/\.js/g, ".min.js"))
-      .pipe(replace(/\.css/g, ".min.css"))
-      .pipe(gulp.dest(DIST));
+  .pipe(replace(/\.js/g, ".min.js"))
+  .pipe(replace(/\.css/g, ".min.css"))
+  .pipe(gulp.dest(DIST));
 });
 
 
 // Copy Bower assets
 gulp.task("copy-bower", ["update"], function() {
   return gulp.src("bower_components/**")
-    .pipe(gulp.dest(DIST_LIB));
+  .pipe(gulp.dest(DIST_LIB));
 });
 
 
@@ -127,48 +128,51 @@ gulp.task("dist", ["copy-bower", "dist:html", "dist:less", "dist:javascript", "d
 // Clean the DIST dir
 gulp.task("clean", function() {
   return gulp.src([DIST, ".build"], {read: false})
-    .pipe(require("gulp-clean")());
+  .pipe(require("gulp-clean")());
 });
 
 
 // Updates the Bower dependencies based on the bower.json file
 gulp.task("update", function(next) {
+  gutil.log("start");
 
   var needsUpdate = false;
 
   gulp.src("bower.json")
-    .pipe(require("gulp-newer")(".build"))
-    .pipe(gulp.dest(".build")) // todo: don't do this if the bower install fails
+    .pipe(gulp.dest("build"))
     .on("close", function() {
+      gutil.log("close");
       if (!needsUpdate) {
         next();
       }
     })
     .on("error", function(error) {
+      gutil.log("error");
       if (!needsUpdate) {
         next(error);
       }
     })
     .on("data", function() {
+      gutil.log("data");
       // updated bower.json
       needsUpdate = true;
       gutil.log("Updating Bower Dependencies");
       require("bower").commands.install([], {}, { interactive: false })
-        .on("end", function () {
-          gutil.log("Bower Dependencies Updated");
-          next();
-        })
-        .on("log", function (log) {
-          if (log.level == "action" && log.id == "install") {
-            gutil.log("Added Bower Dependency: " + log.message);
-          }
-        })
-        .on("error", function (error) {
-          gutil.error("Bower Error:", error);
-          next(error);
-        });
+      .on("end", function () {
+        gutil.log("Bower Dependencies Updated");
+        next();
+      })
+      .on("log", function (log) {
+        if (log.level == "action" && log.id == "install") {
+          gutil.log("Added Bower Dependency: " + log.message);
+        }
+      })
+      .on("error", function (error) {
+        gutil.error("Bower Error:", error);
+        next(error);
+      });
     })
-});
+  });
 
 
 // Server that serves static content from DIST
