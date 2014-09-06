4a4e467c85a36150626bc41d34634f74489c1ff3
Matthias Schmitt <mschmitt@db12.de>
2014-09-06 18:28:49 +0200

    akt 1-3 mit kartenreihenfolge

--- app/js/data2.js
+++ app/js/data2.js
@@ -8,7 +8,7 @@
  {name: "Höhle", akt: "2", Aktion: "Nur mit der Fackel kann man durch die Höhle direkt zu anderen Höhlen gehen.",ver: "true", warn:"Fackel" , bgc:"blue"},
  {name: "Lager", akt: "2", Aktion: "Im Lager des Jäger findest du die Artifakte, die dir der Jäger genommen hat.",hor: "true", bgc:"green"},
  {name: "Machete", akt: "2", Aktion: "Die Machete kann eine Dschungelwand öffnen. Lege die Machete dazu auf die Wand.",ver: "true",imgclass:"symbol"},
- {name: "Pistole", akt: "2", Aktion: "Die Pistole ist alt und hat keine Munition.",hor: "true",ver: "true", imgclass:"symbol", bgc: "gelb"},
+ {name: "Pistole", akt: "2", Aktion: "Die Pistole ist alt und hat nur noch eine Kugel.",hor: "true",ver: "true", imgclass:"symbol", bgc: "gelb"},
  {name: "Schlange", akt: "2", Aktion: "Die Schlange hat ein tödliches Gift und beisst dich. Lege ein Gegengift hier ab, um vor der Schlange geschützt zu sein.", warn:"Gegengift",ver: "true"},
  {name: "Schlange", akt: "2", Aktion: "Die Schlange hat ein tödliches Gift und beisst dich. Lege ein Gegengift hier ab, um vor der Schlange geschützt zu sein.", warn:"Gegengift",ver: "true"},
  {name: "Schlange", akt: "2", Aktion: "Die Schlange hat ein tödliches Gift und beisst dich. Lege ein Gegengift hier ab, um vor der Schlange geschützt zu sein.", warn:"Gegengift"},
@@ -18,7 +18,7 @@
 
  var initActions = [
  {name: "Jäger", akt: "2", Aktion: "Der Jäger klaut dir alle Artefakte und bringt sie zu seinem Lager. Mit der Pistole kannst du sie ihm dort abnehmen.",warn:"Pistole", bgc:"orange", win:"Fluss", bgcwin:"blue",task: true},
- {name: "Kannibalendorf", akt: "2", color : "H",Aktion: "Jane wird von den Kannibalen gefangengenommen. Du kannst Sie erst in Akt III befreien.", warn:"Voodoopuppe", win:"Jane2", bgcwin:"green", task: true},
+ {name: "Kannibalendorf", order: "4", akt: "2", color : "H",Aktion: "Jane wird von den Kannibalen gefangengenommen. Du kannst Sie erst in Akt III befreien.", warn:"Voodoopuppe", win:"Jane2", bgcwin:"green", task: true},
  {name: "Elfenbein", akt: "2", Aktion: "Im Elefantenfriedhof findest du Elfenbein. Für den Zugang benötigst du die Machete.",warn:"Machete", win:"Elfenbein", bgcwin:"green", imgclass:"symbol", task: true},
  {name: "Diamantenmine", akt: "2", color : "H",Aktion: "Du findest in der dunklen Mine grosse Diamenten. Bein Herausholen verlierst du die Fackel.", warn:"Fackel", win:"Diamantenmine", bgcwin:"green", imgclass:"symbol", task: true},
  {name: "Tiger", akt: "2", color : "H",Aktion: "Ein Tiger überrascht euch und jagt euch quer durch den Dschungel auf ein beliebiges leeres Feld. Lege eine Karte vom Stapel auf dieses Feld.", task: true}
--- app/js/data3.js
+++ app/js/data3.js
@@ -21,7 +21,7 @@
  {name: "Kannibalen", akt: "3", color : "H", Aktion: "Die Kannibalen sind eigentlich friedliebend und bewachen das Tal. Du kannst ungehindert an Ihnen vorbeilaufen. Falls sie Jane gefangen halten, kommt Sie mit dir mit.", win:"Jane2", bgcwin:"green", task: true},
  {name: "Jane", akt: "3", Aktion: "Jane hat sich in dich verliebt. Sie will bei dir bleiben. ",win:"Fluss", bgcwin:"blue", warn:"Jane2", bgc:"orange", task: true},
  {name: "Höhle", akt: "3", Aktion: "Die Höhle enthält den Schatz, der von den Kannibalen verehrt wird. Durch diese Höhle könnt ihr mit der Fackel direkt zu anderen Höhlen ziehen. ",win:"Fluss", bgcwin:"blue", warn:"Jane2", bgc:"orange", task: true},
- {name: "Prof Hampton", akt: "3", Aktion: "Janes Vater lebt glücklich und zufrieden bei den Kannibalen. Wenn du ihm beide Kugeln bringst, verlässt er mit euch das Tal.",hor: "true", imgclass:"symbol", task: true}
+ {name: "Prof Hampton", akt: "3", order: "4", Aktion: "Janes Vater lebt glücklich und zufrieden bei den Kannibalen. Wenn du ihm beide Kugeln bringst, verlässt er mit euch das Tal.",hor: "true", imgclass:"symbol", task: true}
  ];
 
 
